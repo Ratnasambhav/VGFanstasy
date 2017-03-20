40334cb49b8784b4c92416f2ac8302f451742969
Ratnasambhav Priyadarshi <ratnasambhva@gmail.com>
2017-03-20 12:02:47 +0530

    Removed extra js and css files

--- item_select.js
+++ item_select.js
@@ -8,30 +8,29 @@ $( document ).ready(function() {
 
 function slotOne(item) {
   const slot1 = document.querySelector('body > div.row.container > div.col.s2.slot1 > img');
-  slot1.src = `file:///Users/ratnasambhavpriyadarshi/Development/VGFantasy/Images/VGPics/VG%20Items/${item}.png`
+  slot1.src = `Images/VGPics/VG Items/${item}.png`;
 }
 
 function slotTwo(item) {
   const slot2 = document.querySelector('body > div.row.container > div.col.s2.slot2 > img');
-  slot2.src = `file:///Users/ratnasambhavpriyadarshi/Development/VGFantasy/Images/VGPics/VG%20Items/${item}.png`
+  slot2.src = `Images/VGPics/VG Items/${item}.png`;
 }
 
 function slotThree(item) {
   const slot3 = document.querySelector('body > div.row.container > div.col.s2.slot3 > img');
-  slot3.src = `file:///Users/ratnasambhavpriyadarshi/Development/VGFantasy/Images/VGPics/VG%20Items/${item}.png`
+  slot3.src = `Images/VGPics/VG Items/${item}.png`;
 }
-
 function slotFour(item) {
   const slot4 = document.querySelector('body > div.row.container > div.col.s2.slot4 > img');
-  slot4.src = `file:///Users/ratnasambhavpriyadarshi/Development/VGFantasy/Images/VGPics/VG%20Items/${item}.png`
+  slot4.src = `Images/VGPics/VG Items/${item}.png`;
 }
 
 function slotFive(item) {
   const slot5 = document.querySelector('body > div.row.container > div.col.s2.slot5 > img');
-  slot5.src = `file:///Users/ratnasambhavpriyadarshi/Development/VGFantasy/Images/VGPics/VG%20Items/${item}.png`
+  slot5.src = `Images/VGPics/VG Items/${item}.png`;
 }
 
 function slotSix(item) {
   const slot6 = document.querySelector('body > div.row.container > div.col.s2.slot6 > img');
-  slot6.src = `file:///Users/ratnasambhavpriyadarshi/Development/VGFantasy/Images/VGPics/VG%20Items/${item}.png`
+  slot6.src = `Images/VGPics/VG Items/${item}.png`;
 }
