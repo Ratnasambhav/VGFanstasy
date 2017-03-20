70d43f3e0163938e76b2104ef8e867a1f655c131
Ratnasambhav Priyadarshi <ratnasambhva@gmail.com>
2017-03-20 12:12:59 +0530

    More Inprovements

--- item_select.js
+++ item_select.js
@@ -7,30 +7,30 @@ $( document ).ready(function() {
 });
 
 function slotOne(item) {
-  const slot1 = document.querySelector('body > div.row.container > div.col.s2.slot1 > img');
+  const slot1 = document.querySelector('body > div.row.container.selected-items.center-align > div.col.m2.s6.slot1 > img');
   slot1.src = `Images/VGPics/VG Items/${item}.png`;
 }
 
 function slotTwo(item) {
-  const slot2 = document.querySelector('body > div.row.container > div.col.s2.slot2 > img');
+  const slot2 = document.querySelector('body > div.row.container.selected-items.center-align > div.col.m2.s6.slot2 > img');
   slot2.src = `Images/VGPics/VG Items/${item}.png`;
 }
 
 function slotThree(item) {
-  const slot3 = document.querySelector('body > div.row.container > div.col.s2.slot3 > img');
+  const slot3 = document.querySelector('body > div.row.container.selected-items.center-align > div.col.m2.s6.slot3 > img');
   slot3.src = `Images/VGPics/VG Items/${item}.png`;
 }
 function slotFour(item) {
-  const slot4 = document.querySelector('body > div.row.container > div.col.s2.slot4 > img');
+  const slot4 = document.querySelector('body > div.row.container.selected-items.center-align > div.col.m2.s6.slot4 > img');
   slot4.src = `Images/VGPics/VG Items/${item}.png`;
 }
 
 function slotFive(item) {
-  const slot5 = document.querySelector('body > div.row.container > div.col.s2.slot5 > img');
+  const slot5 = document.querySelector('body > div.row.container.selected-items.center-align > div.col.m2.s6.slot5 > img');
   slot5.src = `Images/VGPics/VG Items/${item}.png`;
 }
 
 function slotSix(item) {
-  const slot6 = document.querySelector('body > div.row.container > div.col.s2.slot6 > img');
+  const slot6 = document.querySelector('body > div.row.container.selected-items.center-align > div.col.m2.s6.slot6 > img');
   slot6.src = `Images/VGPics/VG Items/${item}.png`;
 }
