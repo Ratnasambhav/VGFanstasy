2788bd5cec2dc5dca8e64ba3dbf7961fffa77453
Ratnasambhav Priyadarshi <ratnasambhav@gmail.com>
2017-03-29 16:41:02 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -8,10 +8,7 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
-$servername = "localhost";
-$username = "id935168_vg";
-$password = "VG12345";
-$database = "id935168_vg";
+
 // Create connection
 $con = mysqli_connect($servername, $username, $password, $database);
 // Check connection
