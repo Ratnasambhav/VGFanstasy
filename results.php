36a6ea06eff544713aa495215553240b804580ba
Ratnasambhav Priyadarshi <ratnasambhav@gmail.com>
2017-03-29 16:40:20 +0530

    Update results.php

--- results.php
+++ results.php
@@ -30,10 +30,7 @@ function toasta() {
     $data = htmlspecialchars($data);
     return $data;
   }
-  $servername = "localhost";
-  $username = "id935168_vg";
-  $password = "VG12345";
-  $database = "id935168_vg";
+
   // Create connection
   $con = mysqli_connect($servername, $username, $password, $database);
   // Check connection
